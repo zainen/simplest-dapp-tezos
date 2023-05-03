2a07498a551a712a04baef63e3bc04415af2769a
Zainen Suzuki <zainensuzuki@Zainens-MacBook-Pro.local>
2023-05-03 16:11:48 -0700

    simplify and add comments and base css

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import { useState } from 'react';
 import { TezosToolkit } from '@taquito/taquito';
-import { getSetBalances, getWallet, mutezToTez } from './helperFunctions';
+import { activateWallet, mutezToTez } from './helperFunctions';
 import { BeaconWallet } from '@taquito/beacon-wallet';
 import { NetworkType, Regions } from "@airgap/beacon-types";
 
 
 function App() {
-  const network = 'https://ghostnet.ecadinfra.com'
+  const network = 'https://ghostnet.ecadinfra.com';
   const defaultMatrixNode = "beacon-node-1.sky.papers.tech";
 
   // Basic States
@@ -20,15 +20,15 @@ function App() {
 
   // Reciever Information
   const [recieverPkh, setRecieverPkh] = useState<string>('');
-  const [recieverBalanceBefore, setRecieverBalanceBefore] = useState(0);
-  const [recieverBalanceAfter, setRecieverBalanceAfter] = useState(0);
+  const [recieverBalanceBefore, setRecieverBalanceBefore] = useState<number>(0);
+  const [recieverBalanceAfter, setRecieverBalanceAfter] = useState<number>(0);
 
   // Amount to send
   const [amount, setAmount] = useState<number>(0);
 
   // Hold Wallet and TezosToolkit instances
-  const [wallet] = useState(new BeaconWallet({ name: "Simple Transfer dApp", preferredNetwork: NetworkType.GHOSTNET, matrixNodes: { [Regions.NORTH_AMERICA_WEST]: [defaultMatrixNode] } }))
-  const [Tezos] = useState(new TezosToolkit(network));
+  const [wallet] = useState<BeaconWallet>(new BeaconWallet({ name: "Simple Transfer dApp", preferredNetwork: NetworkType.GHOSTNET, matrixNodes: { [Regions.NORTH_AMERICA_WEST]: [defaultMatrixNode] } }));
+  const [Tezos] = useState<TezosToolkit>(new TezosToolkit(network));
 
   const sendTez = async () => {
     try {
@@ -49,7 +49,6 @@ function App() {
       setLoading(false);
 
       // update balance of sender
-      getSetBalances(setBalance, Tezos);
       const senderPkh = await Tezos.wallet.pkh()
       setBalance(mutezToTez((await Tezos.rpc.getBalance(senderPkh)).toNumber()))
 
@@ -63,64 +62,81 @@ function App() {
   }
 
   return (
-     <div className='h-screen w-full bg-sky-200'>
+    <div className='h-full w-full bg-sky-200'>
+      {/* TITLE SECTION */}
       <div className='flex flex-col justify-center items-center w-full md:justify-between md:items-start pt-10 px-10 absolute md:flex-row'>
-        {/* TITLE */}
         <div className='bg-orange-100 rounded-md p-4 w-fit h-fit mb-4 md:mb-0'>
           <h1 className='text-xl text-sky-900 font-extrabold'>Simple Transfer dApp</h1>
         </div>
-        {/* Connect Wallet and Account information */}
         <div className='bg-orange-100 rounded-md p-4 w-fit'>
-        {!walletInitialized ? 
-          <div className='flex flex-col items-center'>
-            <button className='border border-sky-400 rounded-md bg-sky-100 px-2' onClick={async () => {
-              const initWallet = await getWallet(wallet, setWalletInitialized);
-              Tezos.setWalletProvider(initWallet);
-
-              const getPkh = await initWallet.getPKH();
-              setPkh(getPkh);
-
-              const getBalance = await Tezos.rpc.getBalance(getPkh);
-              setBalance(mutezToTez(getBalance.toNumber()));
-            }}>Connect your wallet</button>
-          </div> : <div className='flex flex-col items-center'>
-            <p>Connected</p>
-            <p>your pkh is: {pkh}</p>
-            <p>your balance is: {balance}ꜩ</p>
-            <button className='border border-sky-400 rounded-md bg-sky-100 px-2' onClick={async () => {
-              await wallet.client.clearActiveAccount();
-              setWalletInitialized(false);
-              setPkh('');
-              setBalance(0);
-            }}>Disconnect</button>
-          </div>
-        }
+          {/* TODO Connect Wallet and Account information */}
+          {!walletInitialized ? 
+            <div className='flex flex-col items-center'>
+              <button className='border border-sky-400 rounded-md bg-sky-100 px-2' onClick={async () => {
+                const activeWallet = await activateWallet(wallet);
+                Tezos.setWalletProvider(activeWallet);
+                
+                const getPkh = await activeWallet.getPKH();
+                setPkh(getPkh);
+                
+                const getBalance = await Tezos.rpc.getBalance(getPkh);
+                setBalance(mutezToTez(getBalance.toNumber()));
+                
+                setWalletInitialized(true);
+              }}>Connect your wallet</button>
+            </div> : <div className='flex flex-col items-center'>
+              <p>Connected</p>
+              <p>your pkh is: {pkh}</p>
+              <p>your balance is: {balance}ꜩ</p>
+              <button className='border border-sky-400 rounded-md bg-sky-100 px-2' onClick={async () => {
+                await wallet.client.clearActiveAccount();
+                setWalletInitialized(false);
+                setPkh('');
+                setBalance(0);
+              }}>Disconnect</button>
+            </div>
+          }
         </div>
       </div>
       <div className='h-full w-full flex justify-center items-center'>
-        {/* Transaction Interface */}
+        {/* Transaction Interface Container */}
         <div className='bg-orange-100 rounded-md p-4 w-fit flex flex-col items-center'>
-          {error ? <>
-            <pre>
-              <code>{JSON.stringify(error, null, 4)}</code>
-              <button onClick={() => setError(undefined)}>reset</button>
-            </pre>
-          </> : walletInitialized ? 
+          {/* TODO show only error if error */}
+          {error ?
             <>
-              <h1 className='text-lg'>Transfer Tez</h1>
-              <label>Reciever:</label>
-              <input type="text" className='w-4/5 text-center border border-sky-400 rounded-md bg-sky-100 mb-2' onChange={(e) => {setRecieverPkh(e.target.value)}} />
-              <label>Amount in Mutez (1 Tez = 1000000 Mutez):</label>
-              <input type="number" min={0} className='w-4/5 border text-center border-sky-400 rounded-md bg-sky-100 mb-2' onChange={(e) => {setAmount(Number(e.target.value) ? Number(e.target.value) : 0)}} />
-
-              {!loading ? <button className='border border-sky-400 rounded-md bg-sky-100 px-2' onClick={sendTez}>Send</button> : <p className='border border-sky-400 rounded-md bg-sky-100 px-2'>loading...</p>}
-              <p className='pt-2'>test reciever:</p>
-              <p className='pt-2'>tz1RugwuGQsNDRUtP2NZmtXCsqL7TgpXh2Wo</p>
-              {recieverBalanceBefore ? <p className='pt-2'>reciever balance before: {recieverBalanceBefore}ꜩ</p> : <></>}
-              {recieverBalanceAfter ? <p className='pt-2'>reciever balance after: {recieverBalanceAfter}ꜩ</p> : <></>}
+              <pre>
+                <code>{JSON.stringify(error, null, 4)}</code>
+                <button onClick={() => setError(undefined)}>reset</button>
+              </pre>
             </> : 
-            <p>Please connect your wallet </p>
-        }
+            // show only if wallet is initialized
+            walletInitialized ? 
+              <>
+                <h1 className='text-lg'>Transfer Tez</h1>
+                <label>Reciever:</label>
+                <input type="text" className='w-4/5 text-center border border-sky-400 rounded-md bg-sky-100 mb-2' onChange={(e) => {setRecieverPkh(e.target.value)}} />
+                <label>Amount in Mutez (1 Tez = 1000000 Mutez):</label>
+                <input type="number" min={0} className='w-4/5 border text-center border-sky-400 rounded-md bg-sky-100 mb-2' onChange={(e) => {setAmount(Number(e.target.value) ? Number(e.target.value) : 0)}} />
+                {/* TODO button or loading */}
+                {!loading ? 
+                  <button className='border border-sky-400 rounded-md bg-sky-100 px-2' onClick={sendTez}>Send</button> : 
+                  <p className='border border-sky-400 rounded-md bg-sky-100 px-2'>loading...</p>
+                }
+                <p className='pt-2'>test reciever:</p>
+                <p className='pt-2'>tz1RugwuGQsNDRUtP2NZmtXCsqL7TgpXh2Wo</p>
+                {/* TODO add balance before and after */}
+                {recieverBalanceBefore ? 
+                  <p className='pt-2'>reciever balance before: {recieverBalanceBefore}ꜩ</p> : 
+                  <></>
+                }
+                {recieverBalanceAfter ? 
+                  <p className='pt-2'>reciever balance after: {recieverBalanceAfter}ꜩ</p> : 
+                  <></>
+                }
+              </> : 
+              // Prompt to connect wallet if not initialized
+              <p>Please connect your wallet </p>
+          }
         </div>
       </div>
      </div>
--- src/helperFunctions.ts
+++ src/helperFunctions.ts
@@ -1,39 +1,21 @@
-
-import { TezosToolkit } from "@taquito/taquito";
 import { Dispatch, SetStateAction } from "react";
 import { BeaconWallet } from "@taquito/beacon-wallet";
 import { NetworkType } from "@airgap/beacon-types";
 
-export const getSetBalances = async  (setBalance: Dispatch<SetStateAction<number>>, Tezos: TezosToolkit) => {
-  const balance = await Tezos.rpc.getBalance(await Tezos.wallet.pkh());
-  setBalance(balance.toNumber() / 1000000);
-
-}
-
-export const getWallet = async (wallet: BeaconWallet, setWalletInitialized: Dispatch<SetStateAction<boolean>>) => {
+export const activateWallet = async (wallet: BeaconWallet): Promise<BeaconWallet> => {
   const permission = await wallet.client.requestPermissions({ network: { type: NetworkType.GHOSTNET }});
-  if (permission.address) {
-    setWalletInitialized(true);
-    console.log(permission)
+  if (!permission.address) {
+    throw Error("Permission denied")
   }
   return wallet;
 }
 
-export const clearWallet = async (wallet: BeaconWallet, setGetPermisson: Dispatch<SetStateAction<boolean>>, setWalletInitialized: Dispatch<SetStateAction<boolean>>) => {
+export const clearWallet = async (wallet: BeaconWallet, setGetPermisson: Dispatch<SetStateAction<boolean>>, setWalletInitialized: Dispatch<SetStateAction<boolean>>): Promise<void> => {
   await wallet.client.clearActiveAccount();
   setGetPermisson(false);
   setWalletInitialized(false);
-  
 }
 
-export const initWallet = async (wallet: BeaconWallet, Tezos: TezosToolkit) => {
-  const permission = await wallet.client.requestPermissions();
-  if (permission.address) {
-    Tezos.setWalletProvider(wallet);
-  }
-}
-
-
-export const mutezToTez = (mutez: number) => {
+export const mutezToTez = (mutez: number): number => {
   return mutez / 1000000;
 }
